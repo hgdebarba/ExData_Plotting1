6a895afc3e9d09c9527f5d48ca9cca2fb3a478ec
hgdebarba <hgdebarba@gmail.com>
2014-05-06 18:01:30 +0200

    same commit with correct username

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@ alldata = read.csv("household_power_consumption.txt", na.strings = "?", header =
 taskdata = subset(alldata, alldata$Date == "1/2/2007" | alldata$Date == "2/2/2007")
 taskdata$Date = as.Date(taskdata$Date, format = "%d/%m/%Y")
 
-## Create PNG device, and plot the histogram on it
+## Create PNG device,  and plot the histogram on it
 png(filename = "plot1.png")
 hist(taskdata[,3], xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
 dev.off()
